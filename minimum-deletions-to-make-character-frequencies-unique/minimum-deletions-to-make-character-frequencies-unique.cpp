8b3c2cbc72451589b2bc7e393f0ec17a16a9afb3
uditjaitly <jaitlyudit@gmail.com>
2021-05-18 11:11:27 -0400

    Time: 48 ms (97.62%), Space: 17.3 MB (75.51%) - LeetHub

--- minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,52 +1,13 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        map<char,int> track;
-        priority_queue<int> pq;
-        set<int> setTrack;
-        int count=0;
-        for(int i=0;i!=s.size();i++){
-            if(track.find(s[i])==track.end()){
-                track.insert(pair<char,int>(s[i],1));
-            }
-            else{
-                track[s[i]]++;
-            }
-        }    
-        for(auto itr=track.begin();itr!=track.end();itr++){
-            pq.push(itr->second);
-        }
-        
-        while(pq.size()>0){
-            if(setTrack.find(pq.top())==setTrack.end()){
-                cout<<pq.top()<<" ";
-                setTrack.insert(pq.top());
-                pq.pop();
-            }
-            else if(setTrack.find(pq.top())!=setTrack.end() && pq.top()!=1){
-                cout<<pq.top()<<" ";
-                int i=0;
-                while(setTrack.find(pq.top()-i)!=setTrack.end() && pq.top()-i!=0){
-                    cout<<"here";
-                    count++;
-                    i++;
-                }
-                setTrack.insert(pq.top()-i);
-                pq.pop();
-                
-            }
-            else if(pq.top()==1){
-                pq.pop();
-                count++;
-            }
-        }
-        
-        
-        
-        
-        
-        
-        
-        return count;
-    }
+    int cnt[26] = {}, res = 0;
+    unordered_set<int> used;
+    for (char ch : s)
+        ++cnt[ch - 'a'];
+    for (int i = 0; i < 26; ++i)
+        for (; cnt[i] > 0 && !used.insert(cnt[i]).second; --cnt[i])
+            ++res;
+    return res;
+}
 };
